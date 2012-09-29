4be4d74c6a09ca68d0263fdede7668f157cbc2c3
daisuke6810 <daisuke6810@gmail.com>
2012-09-29 19:42:16 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwsHandsOn2::Application.config.secret_token = '0040d3642957f494ce454428308d933fd4c2f29f262f3348efb8657405ccaa1d5d5f8e50951e252de226032d97dbe2b5b42143119e955ef7acd51686e53afc27'
