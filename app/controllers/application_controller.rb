81b4f2ab7dd6144921f563704f66e58592672ace
daisuke6810 <daisuke6810@gmail.com>
2012-09-29 20:26:24 +0900

    first version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  APP_DISP_NAME = "登山届.net"
-
 
 end
--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,7 @@ module AwsHandsOn2
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.i18n.default_locale = :ja
   end
 end
